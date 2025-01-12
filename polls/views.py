79bae90c1ef00edda7592798e8a5b0b833683d71
Wendy Smoak <wsmoak@gmail.com>
2025-01-12 09:30:59 -0500

    Improve the index view
    https://docs.djangoproject.com/en/5.1/intro/tutorial03/#write-views-that-actually-do-something

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
